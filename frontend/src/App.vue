482ba314d024cd90400ceb545c97fc267d217a4d
JSancho <von.sancho@gmail.com>
2025-04-24 19:16:20 +0200

    Fix #25 Recuperación o creación de la lista de Empleados y carga de esta en la store.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,15 @@
 <script>
 import NavBar from "@/components/navegacion/navbar.vue";
 import Footer from "@/components/navegacion/footer.vue";
+import { useEmpleadosStore } from "@/stores/empleados";
 
 export default {
   async mounted() {
-    // TODO: Crear la lógica al montar el componente principal
+    const empleadosStore = useEmpleadosStore();
+
+    empleadosStore.cargarEmpleados();
+
+    // TODO: Asegurar que se guarda la lista de empleados en el store cada vez que se modifica.
   },
 
   components: { NavBar, Footer },
--- frontend/src/stores/empleados.js
+++ frontend/src/stores/empleados.js
@@ -1,9 +1,28 @@
 import { defineStore } from "pinia";
 
 export const useEmpleadosStore = defineStore("empleados", {
-  state: () => ({}),
+  state: () => ({
+    empleados: []
+  }),
 
   getters: {},
 
-  actions: {},
+  actions: {
+    // Se chequea si la lista de empleados se encuentra en el localStorage. Si está, se carga en el store. Si no está, se genera desde el JSON del proyecto.
+    async cargarEmpleados() {
+      const empleadosEnLocalStorage = localStorage.getItem('empleados')
+      if (empleadosEnLocalStorage) {
+        this.empleados = JSON.parse(empleadosEnLocalStorage)
+      } else {
+        const varEmpleadosJson = await import('@/assets/json/empleados.json')
+        this.empleados = varEmpleadosJson._embedded.empleados
+        console.log(this.empleados)
+      }
+    },
+
+    // Guarda en el localStorage los empleados cada vez que se modifica el objeto empleados.
+    guardarEmpleadosEnLocalStorage() {
+      localStorage.setItem('empleados', JSON.stringify(this.empleados));
+    }
+  },
 });
