8767cdf06d7f2a3dea163e58793c9acffc689a08
James Neutron <von.sancho@gmail.com>
2025-05-30 10:14:46 +0200

    Fix #36 Acceder fichero de festivos autonomicos y festivos locales mediante llamada a API solo en caso necesario

--- frontend/src/stores/festivosAPI.js
+++ frontend/src/stores/festivosAPI.js
@@ -1,42 +1,74 @@
 import { defineStore } from "pinia";
-import { getFestivosLocales, getFestivosRegionales } from "@/stores/api-service-festivos";
+import {
+  getFestivosLocales,
+  getFestivosRegionales,
+} from "@/stores/api-service-festivos";
 
 export const useFestivosAPIStore = defineStore("festivosAPI", {
   state: () => {
     return {
       festivos: [],
-      festivosCargados: false
+      festivosCargados: false,
     };
   },
 
-  getters: {
-  },
+  getters: {},
 
   actions: {
     async cargarFestivos() {
-      await getFestivosLocales().then((response) => {
-        if (response.data) {
-          const arrayFestivosParseadosJSON = response.data.data
-          const festivosMadrid = arrayFestivosParseadosJSON.filter((m) => m.municipio_nombre === 'Madrid').map((m) => m.fecha_festivo)
-          this.festivos = festivosMadrid
-        }
-        this.festivosCargados = true
+      this.festivos = [];
 
-      }).catch((error) => {
-        console.warn(`No se ha podido acceder a la API de festivos locales : ${error}`)
-      })
+      if (!localStorage.getItem("festivosLocalesMadridJSON")) {
+        await getFestivosLocales()
+          .then((response) => {
+            if (response.data) {
+              const arrayFestivosParseadosJSON = response.data.data;
+              const festivosMadrid = arrayFestivosParseadosJSON
+                .filter((m) => m.municipio_nombre === "Madrid")
+                .map((m) => m.fecha_festivo);
+              this.festivos = festivosMadrid;
+              localStorage.setItem(
+                "festivosLocalesMadridJSON",
+                JSON.stringify(festivosMadrid)
+              );
+            }
+            this.festivosCargados = true;
+          })
+          .catch((error) => {
+            console.warn(
+              `No se ha podido acceder a la API de festivos locales : ${error}`
+            );
+          });
+      } else {
+        this.festivos = JSON.parse("festivosLocalesMadridJSON");
+        this.festivosCargados = true;
+      }
 
-      await getFestivosRegionales().then((response) => {
-        if (response.data) {
-          const arrayFestivosParseadosJSON = response.data.data
-          const festivosRegionales = arrayFestivosParseadosJSON.map((f) => f.fecha_festivo)
-          this.festivos = this.festivos.concat(festivosRegionales)
-        }
-        this.festivosCargados = true
-
-      }).catch((error) => {
-        console.warn(`No se ha podido acceder a la API de festivos regionales : ${error}`)
-      })
+      if (!localStorage.getItem("festivosRegionalesMadridJSON")) {
+        await getFestivosRegionales()
+          .then((response) => {
+            if (response.data) {
+              const arrayFestivosParseadosJSON = response.data.data;
+              const festivosRegionales = arrayFestivosParseadosJSON.map(
+                (f) => f.fecha_festivo
+              );
+              this.festivos = this.festivos.concat(festivosRegionales);
+              localStorage.setItem(
+                "festivosRegionalesMadridJSON",
+                JSON.stringify(festivosRegionales)
+              );
+            }
+            this.festivosCargados = true;
+          })
+          .catch((error) => {
+            console.warn(
+              `No se ha podido acceder a la API de festivos regionales : ${error}`
+            );
+          });
+      } else {
+        this.festivos.concat("festivosRegionalesMadridJSON");
+        this.festivosCargados = true;
+      } 
     },
   },
 });
